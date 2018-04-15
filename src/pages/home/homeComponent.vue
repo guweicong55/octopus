8529c00416dbe24200609c1fbca32b054b93c15a
guweicong55 <mine@guweicong.local>
2018-04-15 22:47:35 +0800

    11
    1

--- src/pages/home/homeComponent.vue
+++ src/pages/home/homeComponent.vue
@@ -2,7 +2,7 @@
   <div>
     <head-component :searchShow="1"></head-component>
     <div class="article_list_wrapper">
-      <home-list></home-list>
+      <home-list :items="list"></home-list>
     </div>
   </div>
 </template>
@@ -14,6 +14,22 @@
       components: {
         headComponent,
         homeList
+      },
+      data () {
+        return {
+          list: [
+            {
+              id: 1,
+              title: '我们可以为组件的 prop 指定验证规则。如果传入的数据不符合要求，Vue 会发出警告。',
+              author: 'Madonna-Skoni'
+            },
+            {
+              id: 2,
+              title: '要指定验证规则，需要用对象的形式来定义 prop，而不能用字符串数组：',
+              author: '推塔呀呀呀'
+            }
+          ]
+        }
       }
     }
 </script>
